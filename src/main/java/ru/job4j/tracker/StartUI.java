5e51bfe265eb63b335aa23d440b0729409aaebe5
user <none>
2020-06-15 16:16:02 +0500

    4.2. Статические методы. [#282465]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,64 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+
+    public static void createItem(Input inp, Tracker tr) {
+        String addName = inp.askStr("Enter new item name: ");
+        tr.add(new Item(addName));
+    }
+
+    public static void listItem(Tracker tr) {
+        Item[] itarrList = tr.findAll();
+        if (itarrList.length != 0) {
+            for (int i = 0; i < itarrList.length; i++) {
+                System.out.println("[" + itarrList[i].getId() + "]:" + itarrList[i].getName());
+            }
+        } else {
+            System.out.println("Empty");
+        }
+    }
+
+    public static void editItem(Input inp, Tracker tr) {
+        String idEdit = inp.askStr("Enter ID to edit: ");
+        String newName = inp.askStr("New item name: ");
+        if (tr.replace(idEdit, new Item(newName))) {
+            System.out.println("Replace completed.");
+        } else {
+            System.out.println("Error while tried to edit id=" + idEdit);
+        }
+    }
+
+    public static void deleteItem(Input inp, Tracker tr) {
+        String idDelete = inp.askStr("Enter Id to delete: ");
+        if (tr.delete(idDelete)) {
+            System.out.println("Item " + idDelete + " deleted.");
+        } else {
+            System.out.println("Error while tried to delete id=" + idDelete);
+        }
+    }
+
+    public static void findItemById(Input inp, Tracker tr) {
+        String idFind = inp.askStr("Enter id to find: ");
+        Item itmFind = tr.findById(idFind);
+        if (itmFind != null) {
+            System.out.println("found: [" + itmFind.getId() + "]:" + itmFind.getName());
+        } else {
+            System.out.println("Nothing found with id " + idFind);
+        }
+    }
+
+    public void findItemByName(Input in, Tracker tr) {
+        String findName = in.askStr("Enter name to find: ");
+        Item[] itarrFind = tr.findByName(findName);
+        if (itarrFind != null) {
+            for (int i = 0; i < itarrFind.length; i++) {
+                System.out.println("[" + itarrFind[i].getId() + "]:" + itarrFind[i].getName());
+            }
+        } else {
+            System.out.println("Nothing found for \"" + findName + "\"");
+        }
+    }
+
     public void init(Input in, Tracker tr) {
         boolean run = true;
         while (run) {
@@ -8,61 +66,29 @@ public class StartUI {
             int select = in.askInt("");
             switch (select) {
                 case 0:
-                    String addName = in.askStr("Enter new item name: ");
-                    tr.add(new Item(addName));
+                    createItem(in, tr);
                     break;
                 case 1:
-                    Item[] itarrList = tr.findAll();
-                    if (itarrList.length != 0) {
-                        for (int i = 0; i < itarrList.length; i++) {
-                            System.out.println("[" + itarrList[i].getId() + "]:" + itarrList[i].getName());
-                        }
-                    } else {
-                        System.out.println("Empty");
-                    }
+                    listItem(tr);
                     break;
                 case 2:
-                    String idEdit = in.askStr("Enter ID to edit: ");
-                    String newName = in.askStr("New item name: ");
-                    if (tr.replace(idEdit, new Item(newName))) {
-                        System.out.println("Replace completed.");
-                    } else {
-                        System.out.println("Error while tried to edit id=" + idEdit);
-                    }
+                    editItem(in, tr);
                     break;
                 case 3:
-                    String idDelete = in.askStr("Enter Id to delete: ");
-                    if (tr.delete(idDelete)) {
-                        System.out.println("Item " + idDelete + " deleted.");
-                    } else {
-                        System.out.println("Error while tried to delete id=" + idDelete);
-                    }
+                    deleteItem(in, tr);
                     break;
                 case 4:
-                    String idFind = in.askStr("Enter id to find: ");
-                    Item itmFind = tr.findById(idFind);
-                    if (itmFind != null) {
-                        System.out.println("found: [" + itmFind.getId() + "]:" + itmFind.getName());
-                    } else {
-                        System.out.println("Nothing found with id " + idFind);
-                    }
+                    findItemById(in, tr);
                     break;
                 case 5:
-                    String findName = in.askStr("Enter name to find: ");
-                    Item[] itarrFind = tr.findByName(findName);
-                    if (itarrFind != null) {
-                        for (int i = 0; i < itarrFind.length; i++) {
-                            System.out.println("[" + itarrFind[i].getId() + "]:" + itarrFind[i].getName());
-                        }
-                    } else {
-                        System.out.println("Nothing found for \"" + findName + "\"");
-                    }
+                    findItemByName(in, tr);
                     break;
                 case 6: run = false; break;
                 default: continue;
             }
         }
     }
+
 
     private void showMenu() {
         System.out.println("Menu");
